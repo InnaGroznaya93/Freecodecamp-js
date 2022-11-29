7af79539c1212ec1ca85a97a7bfa1ac0c0721786
Inna Groznaya <inna2011@tut.by>
2022-11-29 11:53:35 +0300

    Match Ending String Patterns

--- reg.exp.1.js
+++ reg.exp.1.js
@@ -1,3 +1,13 @@
-let rickyAndCal = "Cal and Ricky both like racing.";
-let calRegex = /^Cal/; // search at the beginning of the line
-let result = calRegex.test(rickyAndCal);
+{
+    //Match Beginning String Patterns
+  let rickyAndCal = "Cal and Ricky both like racing.";
+  let calRegex = /^Cal/; // search at the beginning of the line
+  let result = calRegex.test(rickyAndCal);
+}
+
+{
+    //Match Ending String Patterns
+  let caboose = "The last car on a train is the caboose";
+  let lastRegex = /caboose$/; //search at the end of the line
+  let result = lastRegex.test(caboose);
+}
